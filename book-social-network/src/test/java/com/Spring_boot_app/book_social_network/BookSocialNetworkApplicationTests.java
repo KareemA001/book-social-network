276b835b263009e9f9903ad4ee6166d706ec3cf2
KareemA001 <kalaa8851@gmail.com>
2026-04-19 22:59:46 +0200

    initial commit

---
+++ book-social-network/src/test/java/com/Spring_boot_app/book_social_network/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_boot_app.book_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
